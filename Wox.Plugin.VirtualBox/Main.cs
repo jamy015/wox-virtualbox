2bf31e075fd64a99e9ba1f351a6762563dfaa693
Jamy Mahabier <jamysanjay@gmail.com>
2017-11-25 07:51:12 +0100

    Create project with Wox.Plugin and VirtualBox references

---
+++ Wox.Plugin.VirtualBox/Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using VirtualBox;
+
+namespace Wox.Plugin.VirtualBox {
+    public class Main : IPlugin {
+
+        /// <summary>
+        /// Wox plugin init method
+        /// </summary>
+        /// <param name="context">Wox context</param>
+        public void Init(PluginInitContext context) { }
+
+        /// <summary>
+        /// Wox plugin query method
+        /// </summary>
+        /// <param name="query">Query entered by the user</param>
+        /// <returns>List of results to display</returns>
+        public List<Result> Query(Query query) { }
+
+    }
+}
